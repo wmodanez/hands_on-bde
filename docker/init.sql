08c52492cc08bc87cd0427fb4bed9329323697a5
Wesley Modanez Freitas <wesley.modanez@goias.gov.br>
2026-03-05 11:13:14 -0300

    feat: adicionar bancos de dados e usuários individuais com privilégios específicos para cada usuário.

--- docker/init.sql
+++ docker/init.sql
@@ -1,25 +1,47 @@
--- Create modanez user with full privileges
-CREATE USER IF NOT EXISTS 'modanez'@'%' IDENTIFIED BY '123456';
-GRANT ALL PRIVILEGES ON *.* TO 'modanez'@'%' WITH GRANT OPTION;
-
 -- Create database 'imp'
 CREATE DATABASE IF NOT EXISTS imp;
 
--- Create users with read-only access to 'imp' database and full access to their own databases
+-- Create individual databases for each user
+CREATE DATABASE IF NOT EXISTS bruna;
+CREATE DATABASE IF NOT EXISTS lorenna;
+CREATE DATABASE IF NOT EXISTS rejane;
+CREATE DATABASE IF NOT EXISTS santino;
+CREATE DATABASE IF NOT EXISTS modanez;
+
+-- Create users with read-only access to 'imp' database and full access ONLY to their own databases
 CREATE USER IF NOT EXISTS 'bruna'@'%' IDENTIFIED BY 'b123456';
 GRANT SELECT ON imp.* TO 'bruna'@'%';
-GRANT ALL PRIVILEGES ON bruna.* TO 'bruna'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON bruna.* TO 'bruna'@'%';
+REVOKE ALL PRIVILEGES ON *.* FROM 'bruna'@'%';
+GRANT SELECT ON imp.* TO 'bruna'@'%';
+GRANT ALL PRIVILEGES ON bruna.* TO 'bruna'@'%';
 
 CREATE USER IF NOT EXISTS 'lorenna'@'%' IDENTIFIED BY 'l123456';
 GRANT SELECT ON imp.* TO 'lorenna'@'%';
-GRANT ALL PRIVILEGES ON lorenna.* TO 'lorenna'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON lorenna.* TO 'lorenna'@'%';
+REVOKE ALL PRIVILEGES ON *.* FROM 'lorenna'@'%';
+GRANT SELECT ON imp.* TO 'lorenna'@'%';
+GRANT ALL PRIVILEGES ON lorenna.* TO 'lorenna'@'%';
 
 CREATE USER IF NOT EXISTS 'rejane'@'%' IDENTIFIED BY 'r123456';
 GRANT SELECT ON imp.* TO 'rejane'@'%';
-GRANT ALL PRIVILEGES ON rejane.* TO 'rejane'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON rejane.* TO 'rejane'@'%';
+REVOKE ALL PRIVILEGES ON *.* FROM 'rejane'@'%';
+GRANT SELECT ON imp.* TO 'rejane'@'%';
+GRANT ALL PRIVILEGES ON rejane.* TO 'rejane'@'%';
 
 CREATE USER IF NOT EXISTS 'santino'@'%' IDENTIFIED BY 's123456';
 GRANT SELECT ON imp.* TO 'santino'@'%';
-GRANT ALL PRIVILEGES ON santino.* TO 'santino'@'%' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON santino.* TO 'santino'@'%';
+REVOKE ALL PRIVILEGES ON *.* FROM 'santino'@'%';
+GRANT SELECT ON imp.* TO 'santino'@'%';
+GRANT ALL PRIVILEGES ON santino.* TO 'santino'@'%';
+
+CREATE USER IF NOT EXISTS 'modanez'@'%' IDENTIFIED BY '123456';
+GRANT SELECT ON imp.* TO 'modanez'@'%';
+GRANT ALL PRIVILEGES ON modanez.* TO 'modanez'@'%';
+REVOKE ALL PRIVILEGES ON *.* FROM 'modanez'@'%';
+GRANT SELECT ON imp.* TO 'modanez'@'%';
+GRANT ALL PRIVILEGES ON modanez.* TO 'modanez'@'%';
 
 FLUSH PRIVILEGES;
